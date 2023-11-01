0b7390549f337a7017c34483e584ce1ff235b666
swapnilrkhamkar <swapnilkhamkar@yahoo.in>
2023-11-01 13:21:30 +0530

    First Commit

---
+++ app/src/main/java/com/compose/course/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.compose.course
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.compose.course.ui.theme.JetpackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Row(
+                modifier = Modifier
+                    .width(300.dp)
+                    .fillMaxHeight(0.7f)
+                    .background(Color.Green),
+                horizontalArrangement = Arrangement.SpaceAround,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(text = "Hello")
+                Text(text = "World")
+                Text(text = "Hello")
+            }
+        }
+    }
+}
