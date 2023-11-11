b3c1dfcaa64254dca9f3c8271f940c766dc99128
swapnilrkhamkar <swapnilkhamkar@yahoo.in>
2023-11-11 18:15:14 +0530

    List with LazyColumn and items

--- app/src/main/java/com/compose/course/MainActivity.kt
+++ app/src/main/java/com/compose/course/MainActivity.kt
@@ -3,11 +3,9 @@ package com.compose.course
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
@@ -22,14 +20,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            val scrollState = rememberScrollState()
-            Column(
-                modifier = Modifier
-                    .verticalScroll(scrollState)
-            ) {
-                for (i in 1..50) {
+            LazyColumn {
+                items(5000) {
                     Text(
-                        text = "Item $i",
+                        text = "Item $it",
                         fontSize = 24.sp,
                         fontWeight = FontWeight.Bold,
                         textAlign = TextAlign.Center,
