3d52908347c8d15c0a3d657b00fb5ef3a548d2e3
swapnilrkhamkar <swapnilkhamkar@yahoo.in>
2023-11-11 18:20:59 +0530

    List with LazyColumn and itemsIndexed

--- app/src/main/java/com/compose/course/MainActivity.kt
+++ app/src/main/java/com/compose/course/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
@@ -21,9 +22,11 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             LazyColumn {
-                items(5000) {
+                itemsIndexed(
+                    listOf("This", "is", "jetpack", "compose")
+                ) { index, item ->
                     Text(
-                        text = "Item $it",
+                        text = "$item $index",
                         fontSize = 24.sp,
                         fontWeight = FontWeight.Bold,
                         textAlign = TextAlign.Center,
